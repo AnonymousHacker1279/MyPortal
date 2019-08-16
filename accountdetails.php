03cccf18c4a67e8ebd0798a17c59d4553a3ed399
Anonymous Hacker <48810167+AnonymousHacker1279@users.noreply.github.com>
2019-08-15 20:53:24 -0400

    Added backend files
    This commit includes multiple files for multiple uses:
    
    accountdetails.php:
    Account details, with associated images
    
    index.html:
    Index page linking to login/signup pages
    
    maintenance.html:
    Maintenance page, activated when maintenance1.enable is renamed to maintenance.enable
    
    Stylesheets:
    CSS sheets used for page designs
    
    To-Do List.txt:
    To-Do list
    
    MyPortal Bug Tracker.accdb:
    Microsoft Access Bug Tracker database
    
    Other images used by various apps

---
+++ accountdetails.php
@@ -0,0 +1,51 @@
+﻿<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
+$username = $_SESSION['username'];
+?>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Your Account</title>
+    <link rel="stylesheet" href="style001.css" />
+</head>
+<body>
+    <div class="container">
+        <img src="/code01.jpg" alt="code01" style="width:100%; height:180px; opacity: 0.4;" />
+        <div class="centered" style="font-size:50px">Your Account</div>
+    </div>
+
+	<ul>
+		<li><a href="/login/welcome.php">Home</a></li>
+		<li><a href="/dashboard/dashboard.php">Dashboard</a></li>
+		<li style="float:right"><a href="../login/logout.php"><img src="../logout.png" alt="logout" style="vertical-align:middle"/>Log Out</a></li>
+		<li style="float:right;"><a href="accountdetails.php">Account</a></li>
+	</ul>
+
+    <h2>Hello, <?php echo $username;?>. What yould you like to change today?</h2>
+
+	<div class="container">
+		<a href="login/resetpassword.php"><img src="passkey.png" alt="passkey" style="vertical-align: middle;" /></a>
+		<p style="vertical-align: middle;">Change Password</p>
+    </div>
+
+	<br />
+
+	<br />
+
+	<div class="container">
+		<a href="login/deleteaccount.php"><img src="deleteaccount.png" alt="deleteaccount" style="vertical-align: middle;" /></a>
+		<p style="vertical-align: middle;">Delete Account</p>
+	</div>
+
+	<br />
+
+</body>
+</html>
